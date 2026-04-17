948084bf49e9ad91ad250e48ed7cce1afc67e026
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-04-17 22:54:42 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -4,15 +4,8 @@
     return (tp / pos, tn / neg)
 
 def main():
+
     
-    print(f"Sensitivity: {100 * sens:.2f}%")
-    print(f"Specificity: {100 * spec:.2f}%")
-
-if __name__ == "__main__":
-    main()
-
-
-
 
 
 
