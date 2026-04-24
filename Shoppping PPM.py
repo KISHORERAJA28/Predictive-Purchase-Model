788c57c02e06833d263243bb7d107b5ba9017cb7
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-04-24 23:15:26 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -34,10 +34,6 @@ def main():
     X, y = load_data(sys.argv[1])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
-    
-    sens, spec = evaluate(y_test, preds)
-    
-    print(f"Correct: {(y_test == preds).sum()}")
 if __name__ == "__main__":
     main()
 
