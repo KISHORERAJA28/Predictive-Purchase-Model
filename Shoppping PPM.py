294eda3f265043b078b97d89e4a8aee15f69a601
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-04-29 23:15:31 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -1,22 +1,7 @@
 import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-
-def load_data(filename):
-    
-    df = pd.read_csv(filename)
-            
-                mo = {'Jan':0, 'Feb':1, 'Mar':2, 'Apr':3, 'May':4, 'June':5, 
-                      'Jul':6, 'Aug':7, 'Sep':8, 'Oct':9, 'Nov':10, 'Dec':11}
-                
-                df['Month'] = df['Month'].map(mo)
-                df['VisitorType'] = (df['VisitorType'] == 'Returning_Visitor').astype(int)
-                df['Weekend'] = df['Weekend'].astype(int)
-                df['Revenue'] = df['Revenue'].astype(int)        
-
-        
-    X = df.drop('Revenue', axis=1).values.tolist()
+1
     y = df['Revenue'].tolist()
     
     return X, y
