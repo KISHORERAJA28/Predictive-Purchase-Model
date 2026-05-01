95365c1d5c8d37aff068b37078b5f5a8b9558648
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-01 22:41:44 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -8,13 +8,7 @@ def load_data(filename):
     df = pd.read_csv(filename)
 
     mo = {'Jan':0, 'Feb':1, 'Mar':2, 'Apr':3, 'May':4, 'June':5, 
-          'Jul':6, 'Aug':7, 'Sep':8, 'Oct':9, 'Nov':10, 'Dec':11}
-    
-    df['Revenue'] = df['Revenue'].astype(int)
-
-        
-    X = df.drop('Revenue', axis=1).values.tolist()
-    y = df['Revenue'].tolist()
+          'Jul':6, 'Aug':7, 'Sep':8, 'Oct':9, 'Nov':10, 'Dec':11
     
     return X, y
 
