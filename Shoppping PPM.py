c5a1f6882b083fc1eafbab450aa5cc148c9723d3
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-01 22:41:54 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -7,8 +7,6 @@ def load_data(filename):
     
     df = pd.read_csv(filename)
 
-    mo = {'Jan':0, 'Feb':1, 'Mar':2, 'Apr':3, 'May':4, 'June':5, 
-          'Jul':6, 'Aug':7, 'Sep':8, 'Oct':9, 'Nov':10, 'Dec':11
     
     return X, y
 
