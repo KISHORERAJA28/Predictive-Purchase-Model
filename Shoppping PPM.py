de3e7ff87930490d5bcb88e8fa18da3dfc2ccb1b
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-04 22:28:58 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -11,10 +11,6 @@ def load_data(filename):
           'Jul':6, 'Aug':7, 'Sep':8, 'Oct':9, 'Nov':10, 'Dec':11}
     
     df['Month'] = df['Month'].map(mo)
-    df['VisitorType'] = (df['VisitorType'] == 'Returning_Visitor').astype(int)
-    df['Weekend'] = df['Weekend'].astype(int)
-    df['Revenue'] = df['Revenue'].astype(int)
-
         
     X = df.drop('Revenue', axis=1).values.tolist()
     y = df['Revenue'].tolist()
