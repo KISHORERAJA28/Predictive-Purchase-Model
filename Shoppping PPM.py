1e954421da290129fd6f91244f0dba7b8ac1afbe
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-04 22:29:27 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -14,9 +14,6 @@ def load_data(filename):
 
 def evaluate(actual, predicted):
     
-    tp = sum(1 for a, p in zip(actual, predicted) if a == 1 and p == 1)
-    tn = sum(1 for a, p in zip(actual, predicted) if a == 0 and p == 0)
-    
     pos = sum(actual)
     neg = len(actual) - pos
     
