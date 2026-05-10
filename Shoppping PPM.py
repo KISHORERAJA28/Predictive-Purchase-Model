ea85799f9c9929c0808923d871e44c7fad9f8dba
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-10 22:28:00 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -12,11 +12,6 @@ def load_data(filename):
 
 
 
-import pandas as pd
-import sys
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-
 def load_data(filename):
     
     df = pd.read_csv(filename)
