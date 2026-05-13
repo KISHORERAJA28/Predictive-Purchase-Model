e7b89a5171cb85b4e8ce5e9fa9031e10041b7d8a
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-13 22:55:43 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -22,18 +22,6 @@ def main():
     
     model = KNeighborsClassifier(n_neighbors=1)
     model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-
-    
-    sens, spec = evaluate(y_test, preds)
-    
-    print(f"Correct: {(y_test == preds).sum()}")
-    print(f"Incorrect: {(y_test != preds).sum()}")
-    print(f"Sensitivity: {100 * sens:.2f}%")
-    print(f"Specificity: {100 * spec:.2f}%")
-
-if __name__ == "__main__":
-    main()
   
 
 
