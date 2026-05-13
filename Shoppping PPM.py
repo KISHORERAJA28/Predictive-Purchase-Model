a0655597a0ddb62038d3549b2804e9b838dc711e
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-13 22:56:00 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -3,13 +3,6 @@ import sys
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-def load_data(filename):
-def main():
-    if len(sys.argv) != 2:
-        print("Provide the data file!")
-        return
-
-   
     X, y = load_data(sys.argv[1])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
