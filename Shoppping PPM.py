29ead8909cbbac80c367c92271de620f2be9ba60
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-05-15 23:26:54 +0530

    Update Shoppping PPM.py

--- Shoppping PPM.py
+++ Shoppping PPM.py
@@ -1,15 +1,6 @@
 import pandas as pd
 import sys
     X, y = load_data(sys.argv[1])
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
-
-    
-    model = KNeighborsClassifier(n_neighbors=1)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-
-
-
 
 
 import pandas as pd
